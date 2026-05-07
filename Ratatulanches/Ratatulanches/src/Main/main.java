b4a32668258e24b06d52df60f5988cb1a4c6d457
Eduardo <eduardogabrielsc@gmail.com>
2026-05-07 13:38:20 -0300

    Add files via upload

---
+++ Ratatulanches/Ratatulanches/src/Main/main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Menu.MenuCliente;
+import Menu.MenuPrincipal;
+
+public class main {
+	public static void main(String[] args) {
+	
+		MenuCliente mn = new MenuCliente();
+		
+		mn.exibir();
+		
+	}
+
+}
