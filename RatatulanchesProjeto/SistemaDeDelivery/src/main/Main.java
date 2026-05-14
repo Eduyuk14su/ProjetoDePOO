cd33d0a67b0dd543e7e6ee9a259042ebbbe48e5a
Eduardo <eduardogabrielsc@gmail.com>
2026-05-14 10:54:38 -0300

    Add files via upload
    Arquivos do projeto para os caba

---
+++ RatatulanchesProjeto/SistemaDeDelivery/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import Menu.MenuPrincipal;
+import Sql.Conector;
+
+public class Main {
+	public static void main (String[] args) {
+	        Conector.conectar();
+	   
+		MenuPrincipal MP = new MenuPrincipal();
+		MP.exibir();
+		
+	}
+
+}
---
+++ RatatulanchesProjeto/SistemaDeDelivery/src/model/Produto.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Produto {
+	private int id;
+    private int idrestaurante;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    
+    //Getters e Setters
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    
+    public int getIdRestaurante() {return idrestaurante;}
+    public void setIdRestaurante(int idrestaurante) {this.idrestaurante = idrestaurante;}
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getDescricao()  {return descricao;}
+    public void setDescricao(String descricao) {this.descricao = descricao;}
+
+    public Double getPreco() {return preco;}
+    public void setPreco(Double preco) {this.preco = preco;}
+
+}
