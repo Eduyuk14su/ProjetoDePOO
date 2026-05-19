a73eb2b4500ae5304a44738e5130c72589db32cc
Eduardo <eduardogabrielsc@gmail.com>
2026-05-18 22:10:26 -0300

    Update PainelRestaurante.java

--- RatatulanchesProjeto/SistemaDeDelivery/ProjetoPOO_BancodeDados/src/Menu/PainelRestaurante.java
+++ RatatulanchesProjeto/SistemaDeDelivery/ProjetoPOO_BancodeDados/src/Menu/PainelRestaurante.java
@@ -4,44 +4,43 @@ import java.util.Scanner;
 import model.Restaurante;
 
 public class PainelRestaurante {
-	private Restaurante restaurante;
-	Scanner sc = new Scanner(System.in);
-	
-	public PainelRestaurante(Restaurante restaurante) {
-		this.restaurante = restaurante;
-	}
-	public void exibir() {
-		int opcao
-		
-		do {
-			System.out.println("\n=== Painel do Restaurante ==");
-			System.out.println("Restaurante " + restaurante.getNome());
-			System.out.println("1 - Gerenciar Produtos";
-			System.out.println("2 - Gerenciar Produtos";
-			System.out.println("0 - Voltar";
-			
-			opcao = sc.nextInt();
-				sc.nextLine();
-				
-				switch(opcao) {
-				
-				case 1:
-					MenuProduto mp = new MenuProduto(restaurante);
-					mp.exibir();
-					brak;
-					
-				case 2:
-					MenuEntregador me = new MenuEntregador(restaurante);
-					me.exibir();
-					break;
-					
-				case 0:
-					System.out.println("Saindo Do Painel...");
-					break;
-					
-				default:
-					System.out.println("Opção Invalida");	
-				} while(opcao != 0);
-		}
-	}
+    private Restaurante restaurante;
+    Scanner sc = new Scanner(System.in);
+
+    public PainelRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+    public void exibir() {
+        int opcao;
+
+        do {
+            System.out.println("\n=== Painel do Restaurante ===");
+            System.out.println("Restaurante: " + restaurante.getNome());
+            System.out.println("1 - Gerenciar Produtos");
+            System.out.println("2 - Gerenciar Entregadores");
+            System.out.println("0 - Voltar");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch(opcao) {
+                case 1:
+                    MenuProduto mp = new MenuProduto();
+                    mp.exibir();
+                    break;
+
+                case 2:
+                    MenuEntregador me = new MenuEntregador();
+                    me.exibir();
+                    break;
+
+                case 0:
+                    System.out.println("Saindo do painel...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while(opcao != 0);
+    }
 }
