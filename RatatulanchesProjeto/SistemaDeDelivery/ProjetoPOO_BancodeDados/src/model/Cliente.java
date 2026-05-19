e399eb04882948d438bb637f02479574588b7c7c
Eduardo <eduardogabrielsc@gmail.com>
2026-05-18 21:41:03 -0300

    Update Cliente.java

--- RatatulanchesProjeto/SistemaDeDelivery/ProjetoPOO_BancodeDados/src/model/Cliente.java
+++ RatatulanchesProjeto/SistemaDeDelivery/ProjetoPOO_BancodeDados/src/model/Cliente.java
@@ -6,22 +6,61 @@ public class Cliente {
     private String cpf;
     private String telefone;
     private String email;
-
     
     //Getters e Setters
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
+    // ID
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public String getNome() {return nome;}
-    public void setNome(String nome) {this.nome = nome;}
+    // NOME
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        if (nome.length() <= 100) {
+            this.nome = nome;
+        } else {
+            System.out.println("Nome muito grande!");
+        }
+    }
 
-    public String getCpf() {return cpf;}
-    public void setCpf(String cpf) {this.cpf = cpf;}
+    // CPF
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        if (cpf.matches("\\d{11}")) {
+            this.cpf = cpf;
+        } else {
+            System.out.println("CPF inválido! Deve conter 11 números.");
+        }
+    }
 
-    public String getTelefone() {return telefone;}
-    public void setTelefone(String telefone) {this.telefone = telefone;}
+    // TELEFONE
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        if (telefone.matches("\\d{11}")) {
+            this.telefone = telefone;
+        } else {
+            System.out.println("Telefone inválido!");
+        }
+    }
 
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
+    // EMAIL
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        if (email.length() <= 100 && email.contains("@")) {
+            this.email = email;
+        } else {
+            System.out.println("Email inválido!");
+        }
+    }
 }
-
